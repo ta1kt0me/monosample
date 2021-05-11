75ef506013b73036841dfa6c8759a43003faf884
ta1kt0me <p.wadachi@gmail.com>
2021-05-11 20:59:02 +0900

    rubocop -a

--- config/application.rb
+++ config/application.rb
@@ -1,19 +1,19 @@
-require_relative "boot"
+require_relative 'boot'
 
-require "rails"
+require 'rails'
 # Pick the frameworks you want:
-require "active_model/railtie"
-require "active_job/railtie"
+require 'active_model/railtie'
+require 'active_job/railtie'
 # require "active_record/railtie"
 # require "active_storage/engine"
-require "action_controller/railtie"
-require "action_mailer/railtie"
+require 'action_controller/railtie'
+require 'action_mailer/railtie'
 # require "action_mailbox/engine"
 # require "action_text/engine"
-require "action_view/railtie"
+require 'action_view/railtie'
 # require "action_cable/engine"
 # require "sprockets/railtie"
-require "rails/test_unit/railtie"
+require 'rails/test_unit/railtie'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -39,10 +39,10 @@ module Monosample
 
     config.generators do |g|
       g.test_framework :rspec,
-        fixture: false,
-        view_specs: false,
-        helper_specs: false,
-        routing_specs: false
+                       fixture: false,
+                       view_specs: false,
+                       helper_specs: false,
+                       routing_specs: false
     end
   end
 end
--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,4 +1,4 @@
-worker_processes((ENV['UNICORN_WORKER_PROCESSES'].to_i == 0) ? 1 : ENV['UNICORN_WORKER_PROCESSES'].to_i)
+worker_processes(ENV['UNICORN_WORKER_PROCESSES'].to_i == 0 ? 1 : ENV['UNICORN_WORKER_PROCESSES'].to_i)
 timeout 15
 
 listen(ENV.fetch('RACK_PORT', 3000).to_i)
